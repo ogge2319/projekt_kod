01ba1a05929196e7c7784f4286a54ebda5b77207
ogge2319 <167440664+ogge2319@users.noreply.github.com>
2024-05-15 16:53:04 +0200

    Delete Projektarbete directory

--- Projektarbete/projektuppgift2/projektuppgift/projekt.js
+++
@@ -1,8 +0,0 @@
-var map, mapView;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-    map = new Map("viewDiv", {
-        basemap: "streets",
-        center: [14.3505, 62.0333], // Lämpliga koordinater för mitten av Sverige
-        zoom: 6
-    });
-});
