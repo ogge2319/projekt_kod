fdfc0209a93a791192b8fadc74f94f6bfb684269
ogge2319 <167440664+ogge2319@users.noreply.github.com>
2024-05-26 19:02:37 +0200

    Update projekt.js

--- Projekt SENAST/projekt.js
+++ Projekt SENAST/projekt.js
@@ -25,24 +25,24 @@ require([
     postsLayer = new GraphicsLayer();
     slingaLayer = new GraphicsLayer();
     infoLayer = new GraphicsLayer();
-	farnebofjardenPOILayer = new GraphicsLayer();
-	gysingePOILayer = new GraphicsLayer();
-	hedesundafjardenPOILayer = new GraphicsLayer();
+    farnebofjardenPOILayer = new GraphicsLayer();
+    gysingePOILayer = new GraphicsLayer();
+    hedesundafjardenPOILayer = new GraphicsLayer();
 
     map.addLayer(postsLayer);
     map.addLayer(slingaLayer);
     map.addLayer(infoLayer);
-	map.addLayer(farnebofjardenPOILayer);
-	map.addLayer(gysingePOILayer);
-	map.addLayer(hedesundafjardenPOILayer);
+    map.addLayer(farnebofjardenPOILayer);
+    map.addLayer(gysingePOILayer);
+    map.addLayer(hedesundafjardenPOILayer);
 
     // Hide all layers initially
     postsLayer.hide();
     slingaLayer.hide();
     infoLayer.hide();
-	farnebofjardenPOILayer.hide();
-	gysingePOILayer.hide();
-	hedesundafjardenPOILayer.hide();
+    farnebofjardenPOILayer.hide();
+    gysingePOILayer.hide();
+    hedesundafjardenPOILayer.hide();
 
     // Clear markers
     function clearMarkers() {
@@ -64,9 +64,9 @@ require([
         postsLayer.show();
         slingaLayer.hide();
         infoLayer.hide();
-		farnebofjardenPOILayer.hide();
-		gysingePOILayer.hide();
-		hedesundafjardenPOILayer.hide();
+        farnebofjardenPOILayer.hide();
+        gysingePOILayer.hide();
+        hedesundafjardenPOILayer.hide();
 
         fetchData("Etapp_11_wgs84.json").then(showPosts);
         fetchData("Etapp_12_wgs84.json").then(showPosts);
@@ -110,9 +110,9 @@ require([
         slingaLayer.show();
         postsLayer.hide();
         infoLayer.hide();
-		farnebofjardenPOILayer.hide();
-		gysingePOILayer.hide();
-		hedesundafjardenPOILayer.hide();
+        farnebofjardenPOILayer.hide();
+        gysingePOILayer.hide();
+        hedesundafjardenPOILayer.hide();
 
         fetchSlinga("Etapp_Slinga_11_1_wgs84.json").then(showSlinga);
         fetchSlinga("Etapp_Slinga_12_1_wgs84.json").then(showSlinga);
@@ -158,9 +158,9 @@ require([
         infoLayer.show();
         postsLayer.hide();
         slingaLayer.hide();
-		farnebofjardenPOILayer.hide();
-		gysingePOILayer.hide();
-		hedesundafjardenPOILayer.hide();
+        farnebofjardenPOILayer.hide();
+        gysingePOILayer.hide();
+        hedesundafjardenPOILayer.hide();
 
         fetchInfo("Biking_elevation161008.json").then(showInfo);
         fetchInfo("Walk_elevation_123547.json").then(showInfo);
@@ -192,11 +192,8 @@ require([
         infoLayer.add(graphic);
     }
 
-
-
-
-	//FarnebofjardenPOI
-	async function fetchFarnebofjardenPOI(file) {
+    //FarnebofjardenPOI
+    async function fetchFarnebofjardenPOI(file) {
         const url = "https://raw.githubusercontent.com/ogge2319/projekt_kod/main/POI_JSON/FarnebofjardenPOI/";
         const response = await fetch(url + file);
         return response.json();
@@ -206,34 +203,83 @@ require([
         clearMarkers(); // Clear existing markers
         farnebofjardenPOILayer.clear();
         farnebofjardenPOILayer.show();
-		postsLayer.hide();
+        postsLayer.hide();
         slingaLayer.hide();
         infoLayer.hide();
-		gysingePOILayer.hide();
-		hedesundafjardenPOILayer.hide();
+        gysingePOILayer.hide();
+        hedesundafjardenPOILayer.hide();
 
         fetchFarnebofjardenPOI("BarbyHallan.JSON").then(showFarnebofjardenPOI);
     }
 
     function showFarnebofjardenPOI(data) {
         data.posts.forEach(post => {
-            const symbol = new PictureMarkerSymbol("bathPlace.png", 30, 30);
+            let iconUrl;
+            switch (post.name) {
+                case "Bårbyhällan":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Båtsportklubben":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Brattnäset":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Dragsheden öst":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Dragsheden väst":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Göknäset":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Östa Camping":
+                    iconUrl = "camping.png";
+                    break;
+                case "Östa norr":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Östa väst":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Sandön":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Skekarsbo":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Strångnäs":
+                    iconUrl = "bathPlace.png";
+                    break;
+                // Lägg till fler fall för andra POI-namn här
+                default:
+                    iconUrl = "default.png";
+                    break;
+            }
+
+            const symbol = new PictureMarkerSymbol(iconUrl, 30, 30);
             const lat = post.lat;
             const lng = post.lon;
-			const namn = post.name;
+            const namn = post.name;
+            const image = post.image;
+            const description = post.description;
+
             var p = new Point(lng, lat);
             var graphic = new Graphic(p, symbol);
 
             var info = new InfoTemplate();
-            info.setTitle("Färnebofjärden");
-			info.setContent(namn);
+            info.setTitle(namn);
+            info.setContent("<img src='" + image + "' alt='" + namn + "' style='width:100%;'><p>" + description + "</p>");
+
             graphic.setInfoTemplate(info);
             farnebofjardenPOILayer.add(graphic);
         });
     }
-	
-	//GysingePOI
-	async function fetchGysingePOI(file) {
+
+
+    //GysingePOI
+    async function fetchGysingePOI(file) {
         const url = "https://raw.githubusercontent.com/ogge2319/projekt_kod/main/POI_JSON/GysingePOI/";
         const response = await fetch(url + file);
         return response.json();
@@ -243,35 +289,63 @@ require([
         clearMarkers(); // Clear existing markers
         gysingePOILayer.clear();
         gysingePOILayer.show();
-		postsLayer.hide();
+        postsLayer.hide();
         slingaLayer.hide();
         infoLayer.hide();
-		farnebofjardenPOILayer.hide();
-		hedesundafjardenPOILayer.hide();
-		
+        farnebofjardenPOILayer.hide();
+        hedesundafjardenPOILayer.hide();
+
 
         fetchGysingePOI("CafeUdden.JSON").then(showGysingePOI);
     }
 
     function showGysingePOI(data) {
         data.posts.forEach(post => {
-            const symbol = new PictureMarkerSymbol("camping.png", 30, 30);
+            let iconUrl;
+            switch (post.name) {
+                case "Cafe Udden":
+                    iconUrl = "cafe.png";
+                    break;
+                case "Edsviken":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Gärdsvekarna":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Gysinge":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Ista":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Karlhomen":
+                    iconUrl = "windShelter.png";
+                    break;
+            }
+
+            const symbol = new PictureMarkerSymbol(iconUrl, 30, 30);
             const lat = post.lat;
             const lng = post.lon;
-			const namn = post.name;
+            const namn = post.name;
+            const image = post.image;
+            const description = post.description;
+
             var p = new Point(lng, lat);
             var graphic = new Graphic(p, symbol);
 
             var info = new InfoTemplate();
-            info.setTitle("Gysinge");
-			info.setContent(namn);
+            info.setTitle(namn);
+            info.setContent("<img src='" + image + "' alt='" + namn + "' style='width:100%;'><p>" + description + "</p>");
+
             graphic.setInfoTemplate(info);
             gysingePOILayer.add(graphic);
         });
     }
-	
-	//HedesundafjardenPOI
-	async function fetchHedesundafjardenPOI(file) {
+
+
+
+    //HedesundafjardenPOI
+    async function fetchHedesundafjardenPOI(file) {
         const url = "https://raw.githubusercontent.com/ogge2319/projekt_kod/main/POI_JSON/HedesundafjardenPOI/";
         const response = await fetch(url + file);
         return response.json();
@@ -281,42 +355,115 @@ require([
         clearMarkers(); // Clear existing markers
         hedesundafjardenPOILayer.clear();
         hedesundafjardenPOILayer.show();
-		postsLayer.hide();
+        postsLayer.hide();
         slingaLayer.hide();
         infoLayer.hide();
-		farnebofjardenPOILayer.hide();
-		gysingePOILayer.hide();
+        farnebofjardenPOILayer.hide();
+        gysingePOILayer.hide();
 
         fetchHedesundafjardenPOI("Ashuvudet.JSON").then(showHedesundafjardenPOI);
     }
 
     function showHedesundafjardenPOI(data) {
         data.posts.forEach(post => {
-            const symbol = new PictureMarkerSymbol("windshelter.png", 30, 30);
+            let iconUrl;
+            switch (post.name) {
+                case "Åshuvudet":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Festplatsen":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Gnupe":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Hade":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Korsnäset":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Kvillanudden":
+                    iconUrl = "windShelter.png";
+                    break;
+                case "Norra Sundet":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Östveda":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Sandsnäsbadet":
+                    iconUrl = "bathPlace.png";
+                    break;
+                case "Södra Sundet":
+                    iconUrl = "bathPlace.png";
+                    break;
+            }
+
+            const symbol = new PictureMarkerSymbol(iconUrl, 30, 30);
             const lat = post.lat;
             const lng = post.lon;
-			const namn = post.name;
+            const namn = post.name;
+            const image = post.image;
+            const description = post.description;
+
             var p = new Point(lng, lat);
             var graphic = new Graphic(p, symbol);
 
             var info = new InfoTemplate();
-            info.setTitle("Hedesundafjärden");
-			info.setContent(namn);
-            graphic.setInfoTemplate(info);			
+            info.setTitle(namn);
+            info.setContent("<img src='" + image + "' alt='" + namn + "' style='width:100%;'><p>" + description + "</p>");
+
+            graphic.setInfoTemplate(info);
             hedesundafjardenPOILayer.add(graphic);
         });
     }
-	
+
+    map.on("click", function (event) {
+        // Check if the click event occurred within the map's extent
+        if (map.extent.contains(event.mapPoint)) {
+            // Here we'll check if the clicked location already has a POI marker.
+            var alreadyHasPOI = false;
+
+            // We need to loop through each layer and check if there's a graphic at the event.mapPoint
+            var layersToCheck = [farnebofjardenPOILayer, gysingePOILayer, hedesundafjardenPOILayer, postsLayer, slingaLayer, infoLayer];
+            layersToCheck.forEach(function (layer) {
+                var graphics = layer.graphics;
+                graphics.forEach(function (graphic) {
+                    if (graphic.geometry.type === "point") {
+                        var screenPoint = map.toScreen(graphic.geometry);
+                        var distance = Math.sqrt(Math.pow(screenPoint.x - event.screenPoint.x, 2) + Math.pow(screenPoint.y - event.screenPoint.y, 2));
+                        console.log("Checking distance:", distance, "for graphic at", screenPoint, "with click at", event.screenPoint);
+                        if (distance <= graphic.symbol.width / 2) {
+                            alreadyHasPOI = true;
+                            console.log("POI found, no marker added.");
+                        }
+                    }
+                });
+            });
+
+            if (!alreadyHasPOI) {
+                console.log("No POI found, adding marker.");
+                // If no POI is present at this location, add a new custom marker
+                var customMarkerSymbol = new PictureMarkerSymbol("pinpoint.png", 30, 30);
+                var clickedPoint = new Point(event.mapPoint.getLongitude(), event.mapPoint.getLatitude());
+                var customMarkerGraphic = new Graphic(clickedPoint, customMarkerSymbol);
+                map.graphics.add(customMarkerGraphic);
+            }
+        }
+    });
+
+
     // Event listeners
     document.getElementById("fetchDataButton").addEventListener("click", getPostsData);
     document.getElementById("fetchBikinWithoutButton").addEventListener("click", getPostsData);
-	
+
     document.getElementById("fetchSligaButton").addEventListener("click", getPostsSlinga);
-	
+
     document.getElementById("fetchInfoButton").addEventListener("click", getPostsInfo);
     document.getElementById("fetchBikingWithButton").addEventListener("click", getPostsInfo);
-	
-	document.getElementById("fetchFarnebofjardenPOIButton").addEventListener("click", getPostsFarnebofjardenPOI);
-	document.getElementById("fetchGysingePOIPOIButton").addEventListener("click", getPostsGysingePOI);
-	document.getElementById("fetchHedesundafjardenPOIButton").addEventListener("click", getPostsHedesundafjardenPOI);
+
+    document.getElementById("fetchFarnebofjardenPOIButton").addEventListener("click", getPostsFarnebofjardenPOI);
+    document.getElementById("fetchGysingePOIPOIButton").addEventListener("click", getPostsGysingePOI);
+    document.getElementById("fetchHedesundafjardenPOIButton").addEventListener("click", getPostsHedesundafjardenPOI);
 });
